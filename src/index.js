94f39b55d13b9862e5ce85fbcebe330f2cb6eb91
Shawn Hu <shawnhu97@gmail.com>
2022-01-07 18:37:02 -0500

    adding some public files

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(express.static('public'));
+
+// app.get('/', (req, res) => {
+//   res.send('Hello World!');
+// });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
