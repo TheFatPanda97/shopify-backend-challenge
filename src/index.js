4e653bdc5cd0f3ab11764fc4ea23fb63c773ee8b
Shawn Hu <shawnhu97@gmail.com>
2022-01-07 18:32:40 -0500

    added eslint

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import express from 'express';
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
-//runkit.com/embed/save
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
