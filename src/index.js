9aa8e2cfab2a277e43e7da88fdd0e067c2f680b2
Shawn Hu <shawnhu97@gmail.com>
2022-01-07 23:40:45 -0500

    updated api structure

--- src/index.js
+++ src/index.js
@@ -17,7 +17,20 @@ app.use(express.static('public'));
 app.get('/api/inventory', async (req, res) => {
   try {
     const result = await pool.query('SELECT * FROM inventory;');
-    res.json(result.rows);
+    res.json(
+      result.rows.reduce(
+        (acc, curr) => ({
+          ...acc,
+          [curr.id]: {
+            name: curr.name,
+            costPerUnit: curr.cost_per_unit,
+            stock: curr.stock,
+            type: curr.type,
+          },
+        }),
+        {},
+      ),
+    );
   } catch (err) {
     console.log(err);
   }
